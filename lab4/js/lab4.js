f1743c1deac008177efd9a55426af60f57fde1e4
yookakim <yookakim7259@gmail.com>
2021-01-25 17:32:04 -0800

    Completed lab 4 and lab 4 html/style

---
+++ lab4/js/lab4.js
@@ -0,0 +1,11 @@
+// Ask the player to input a number range for the computer to choose a random number from
+  // Display input text field where player determines number range
+  // Store the inputted minimum and maximum values into memory as range variables
+// Choose a random number and store into memory as computer number
+// Ask the player to make a guess for the stored number
+  // Display input text field asking for the player’s guess
+// Receive player input and store into memory as player number
+// Compare the player number with the computer number
+// Notify player of victory if the numbers are matching. If numbers are not matching, notify player of defeat
+// Ask and wait for any player input, then start over from step 1 upon input
+  // Clear and reset variables and UI fields when starting over
