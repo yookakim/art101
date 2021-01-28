f8041396c2cfd5dcd4a5aa0b6ecea8828cabb1de
yookakim <yookakim7259@gmail.com>
2021-01-27 19:02:04 -0800

    commit and push lab5 materials

---
+++ lab5/js/lab.js
@@ -0,0 +1,17 @@
+/*
+ * Author: Yooha Kim (ykim115@ucsc.edu)
+ * Created: Jan 27 2021
+ * License: Public Domain
+ */
+
+ make = "Toyota";
+ model = "Camry";
+ color = "Silver";
+ year = 2010;
+ age = 2021 - year;
+
+ document.writeln("Make: " + make + "<br>");
+ document.writeln("Model: " + model + "<br>");
+ document.writeln("Color: " + color + "<br>");
+ document.writeln("Year: " + year + "<br>");
+ document.writeln("Age: " + age + " years<br>");
