3acfe941796e8c311992ad6517a1ca9bb2b153a9
yookakim <yookakim7259@gmail.com>
2021-03-09 10:49:45 -0800

    lab16 commit

--- lab16/js/lab16.js
+++ lab16/js/lab16.js
@@ -3,10 +3,20 @@ Lab 16: JSON and APIs
 Public Domain
 */
 
+var imgElement = document.getElementById("image");
+var imgTitle = document.getElementById("imagetitle");
+var img = document.createElement("img");
+
 var comicObj = $.ajax({
   type: "GET",
-  url: "http://xkcd.com/info.0.json",
+  url: "https://xkcd.com/info.0.json",
   success: function(data) {
     console.log(data);
+    img.src = data.img;
+    img.alt = data.alt;
+    img.title = data.title;
+    imgTitle.innerHTML = data.title;
   }
 });
+
+imgElement.appendChild(img);
